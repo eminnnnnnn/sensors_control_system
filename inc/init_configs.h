c86ee56cfcb3901912f0aba74e16f6fae2523611
g4d2h13v <e.gadzhiev.mhk@gmail.com>
2020-12-19 19:31:51 +0300

    Removed unnecessary comments

--- inc/init_configs.h
+++ inc/init_configs.h
@@ -1,29 +1,11 @@
 #ifndef INIT_CONFIGS_H
 #define INIT_CONFIGS_H
 
-/*
-typedef struct
-{
-    MDR_PORT_TypeDef * port_name;
-    uint16_t port_pins;
-} mdr_port;
-*/
-
 typedef enum
 {
     INIT_SUCCESS,
     INIT_ERROR
 } init_status_t;
-
-/*
-enum CPU_CLOCK_SRC
-{
-    HSE_SRC,
-    LSE_SRC,
-    LSI_SRC,
-    HSI_SRC
-}
-*/
 
 //////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -38,11 +20,9 @@ init_status_t cpu_clock_init(void);
 
 /*
 ** Описание: Начальная конфигурация портов для LCD дисплея
-** // Параметры: ports - конфигурируемые порты, ports_count - количество портов
 ** Параметры: none
 ** Возвращаемое значение: результат настройки INIT_SUCCESS или INIT_ERROR
 */
-// init_status_t lcd_ports_init(const mdr_port * ports, const uint8_t ports_count);
 init_status_t lcd_ports_init(void);
 
 /*
@@ -50,7 +30,6 @@ init_status_t lcd_ports_init(void);
 ** Параметры: none
 ** Возвращаемое значение: результат настройки INIT_SUCCESS или INIT_ERROR
 */
-// init_status_t button_ports_init(const mdr_port * ports, uint8_t ports_count);
 init_status_t button_ports_init(void);
 
 
