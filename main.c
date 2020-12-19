7bb51a84725497f30f658ae61f4d712d031aa34b
g4d2h13v <e.gadzhiev.mhk@gmail.com>
2020-12-19 21:45:10 +0300

    Simple authenticate

--- main.c
+++ main.c
@@ -24,7 +24,57 @@ int main(void)
 
 	LcdInit();
 	LcdOnAndClear();
-
+	
+	uint8_t passw[4] = {1, 2, 3, 4};
+	uint8_t user_passw[4] = {0, 0, 0, 0}; // 1 5 21 - сдвиги херня идея
+	uint8_t is_auth = 0;
+	
+	while (is_auth == 0)
+	{
+		for (int i = 0; i < 4; ++i)
+			user_passw[i] = 0;
+		int i = 0;
+		while (i < 4)
+		{
+			lcd_print_string("       ", 0, NOT_SMOOTH, NOT_INVERTED);
+			if (PORT_ReadInputDataBit(MDR_PORTB, PORT_Pin_5) == Bit_RESET)
+			{
+				user_passw[i++] = 1;
+				lcd_print_string("up", 0, NOT_SMOOTH, NOT_INVERTED);
+			}
+			else if (PORT_ReadInputDataBit(MDR_PORTB, PORT_Pin_6) == Bit_RESET)
+			{
+				user_passw[i++] = 2;
+				lcd_print_string("right", 0, NOT_SMOOTH, NOT_INVERTED);
+			}
+			else if (PORT_ReadInputDataBit(MDR_PORTE, PORT_Pin_1) == Bit_RESET)
+			{
+				user_passw[i++] = 3;
+				lcd_print_string("down", 0, NOT_SMOOTH, NOT_INVERTED);
+			}
+			else if (PORT_ReadInputDataBit(MDR_PORTE, PORT_Pin_3) == Bit_RESET)
+			{
+				user_passw[i++] = 4;
+				lcd_print_string("left", 0, NOT_SMOOTH, NOT_INVERTED);
+			}
+			mdr_delay_ms(170);
+		}
+		for (i = 0; i < 4; ++i)
+		{
+			if (user_passw[i] != passw[i])
+			{
+				lcd_print_string("try again", 0, NOT_SMOOTH, NOT_INVERTED);
+				mdr_delay_ms(5000);
+				lcd_print_string("         ", 0, NOT_SMOOTH, NOT_INVERTED);
+				is_auth = 0;
+				break;
+			}
+		}
+		if (i == 4)
+			is_auth = 1;
+	}
+	lcd_print_string("success", 0, NOT_SMOOTH, NOT_INVERTED);
+	mdr_delay_ms(5000);
 	menu_rows_t menu_cursor = ROW_0;
 	lcd_menu_init();
 	while(1){
