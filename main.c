cb18ca4272d455466f2a869ac51fc4290f6e69a2
g4d2h13v <e.gadzhiev.mhk@gmail.com>
2020-12-20 15:09:26 +0300

    Program with auth using eeprom r/w interface

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main(void)
 {
 	RST_CLK_DeInit();
 	cpu_clock_init();
+	// SystemCoreClockUpdate();
 	RST_CLK_PCLKcmd(RST_CLK_PCLK_RST_CLK, ENABLE);
 	RST_CLK_PCLKcmd(RST_CLK_PCLK_PORTA
 					| RST_CLK_PCLK_PORTB
@@ -20,6 +21,7 @@ int main(void)
 					| RST_CLK_PCLK_PORTC,
 					ENABLE
 				   );
+	RST_CLK_PCLKcmd(RST_CLK_PCLK_EEPROM, ENABLE);
 	
 	lcd_ports_init();
 	button_ports_init();
@@ -32,29 +34,40 @@ int main(void)
 	while (PORT_ReadInputDataBit(MDR_PORTC, PORT_Pin_2) == Bit_SET);
 	lcd_print_string("              ", 3, NOT_SMOOTH, NOT_INVERTED);
 	lcd_print_string("            ", 4, NOT_SMOOTH, NOT_INVERTED);
-	
+
 	auth_status_t is_auth = AUTH_FAIL;
 	read_password_from_mem();
-	while (is_auth == AUTH_FAIL)
+	if (is_password_defined() == PASSWORD_UNDEFINED)
 	{
+		lcd_print_string("DEFINE PASSWORD", 3, NOT_SMOOTH, NOT_INVERTED);
+		mdr_delay_ms(2000);
+		lcd_print_string("               ", 3, NOT_SMOOTH, NOT_INVERTED);
 		lcd_show_auth_arrows();
-		is_auth = authenticate();
-		if (is_auth == AUTH_FAIL)
-		{
-			lcd_hide_auth_arrows();
-			// LcdPutChar(sym_sp, UP_ARROW_X, UP_ARROW_Y, NOT_SMOOTH, NOT_INVERTED);
-			lcd_print_string("   INCORRECT!", 3, NOT_SMOOTH, NOT_INVERTED);
-			lcd_print_string("   TRY AGAIN", 4, NOT_SMOOTH, NOT_INVERTED);
-			mdr_delay_ms(2000);
-			lcd_print_string("             ", 3, NOT_SMOOTH, NOT_INVERTED);
-			lcd_print_string("            ", 4, NOT_SMOOTH, NOT_INVERTED);
-		}
+		input_new_password();
 	}
+	else
+	{
+		while (is_auth == AUTH_FAIL)
+		{
+			lcd_show_auth_arrows();
+			is_auth = authenticate();
+			if (is_auth == AUTH_FAIL)
+			{
+				lcd_hide_auth_arrows();
+				// LcdPutChar(sym_sp, UP_ARROW_X, UP_ARROW_Y, NOT_SMOOTH, NOT_INVERTED);
+				lcd_print_string("   INCORRECT!", 3, NOT_SMOOTH, NOT_INVERTED);
+				lcd_print_string("   TRY AGAIN", 4, NOT_SMOOTH, NOT_INVERTED);
+				mdr_delay_ms(2000);
+				lcd_print_string("             ", 3, NOT_SMOOTH, NOT_INVERTED);
+				lcd_print_string("            ", 4, NOT_SMOOTH, NOT_INVERTED);
+			}
+		}
+	}	
 	lcd_hide_auth_arrows();
 	lcd_print_string("    SUCCESS!", 3, NOT_SMOOTH, NOT_INVERTED);
 	mdr_delay_ms(2000);
 	lcd_print_string("            ", 3, NOT_SMOOTH, NOT_INVERTED);
-	
+
 	menu_rows_t menu_cursor = ROW_0;
 	lcd_menu_init();
 	while(1){
@@ -65,6 +78,10 @@ int main(void)
 		else if (PORT_ReadInputDataBit(MDR_PORTE, PORT_Pin_1) == Bit_RESET)
 		{
 			lcd_menu_handler(&menu_cursor, MENU_DOWN);
+		}
+		else if (PORT_ReadInputDataBit(MDR_PORTC, PORT_Pin_2) == Bit_RESET)
+		{
+			lcd_menu_handler(&menu_cursor, MENU_SELECT);
 		}
 
 		mdr_delay_ms(150);
